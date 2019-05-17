aed800950cc012674f4ab9f7573e946a7832eabd
mustofaalisahid <38485931+mustofaalisahid@users.noreply.github.com>
2019-05-17 19:31:54 +0700

    Add files via upload

---
+++ latihan 2.c
@@ -0,0 +1,18 @@
+//judul : latihan ke 2
+//nama : mustofa alisahid
+//alamat : sri rejeki timur vii
+//ttl : semarang, 12 nov 1996
+//17 sept 2015
+//oleh mustofa
+#include "stdio.h"
+//kamus
+
+//diskripsi
+main()
+{
+    printf("Biodata\n");
+    printf("Nama : Mustofa Alisahid\n");
+    printf("Alamat : Sri Rejeki Timur VII\n");
+    printf("TTL : Semarang, 12 Nov 1996");
+}
+// printf digunakan untuk menampilkan (print) sebuah kalimat
