748f5f2fa055f8676f98464511e75db49e9da628
mustofaalisahid <38485931+mustofaalisahid@users.noreply.github.com>
2019-05-17 19:58:46 +0700

    Update 3.scan_input_int_float.c

--- 3.scan_input_int_float.c
+++ 3.scan_input_int_float.c
@@ -7,29 +7,23 @@
 //kamus
 int angka1; //variabel int buat bil bulat
 int angka2; //gak boleh ada spasi lho
-float pecahan1, pecahan2, jml; //2 langsung bisa, trs float buat masukin pecahan / desimal
+float pecahan1, pecahan2, jml; //2 langsung bisa, trs float buat masukin pecahan
 
 //deskripsi
 int main()
 {
-    //bilangan bulat
-    angka1=10; //angka dalam variabel
-    angka2=20;
-    jml=angka1+angka2;
-
     //input
-    printf("Lihat angka 1 =\n");
-    scanf("%d", &angka1); // ini dibaca : input kedalam variabel angka1 yang bertipe int (nanti ketika di run, maka bisa di input nilai int / bilangan bulat)
+    printf("Lihat angka 1 = ");
+    scanf("%d", &angka1);
     //tampilan
     printf("Angka 1 = %d\n", angka1);
     //%d digunakan buat km pengen naruh variabelnya dimana, buat int
     //,angka1 itu berati variabel yg pengen mbok munculin yg mana
-    printf("Lihat angka 2 =\n");
+    printf("Lihat angka 2 = ");
     scanf("%d", &angka2);
     printf("Angka 2 = %d\n", angka2);
 
-    printf("Jumlah angka =\n");
-    scanf("%f", &jml); // ini dibaca : input kedalam variabel jml yang bertipe float (nanti ketika di run, maka bisa di input nilai float / desimal)
+    jml=angka1+angka2;
     printf("Angka 1 + Angka 2 = %.2f\n", jml);
     //jangan lupa titik koma yaa
 
@@ -39,13 +33,12 @@ int main()
     pecahan1=12.5;
     pecahan2=7.75;
 
-    printf("Lihat pecahan 1 =\n");
+    printf("Lihat pecahan 1 = ");
     scanf("%f\n", &pecahan1);
     //%f digunakan buat km mau naruh variabelnya dimana, buat float
-    printf("Lihat pecahan 2 =\n");
+    printf("Lihat pecahan 2 = ");
     scanf("%f\n", &pecahan2);
-    printf("Jumlah pecahan =\n");
-    scanf("%f\n", &jml);
+    printf("Jumlah pecahan = %.2f", pecahan1+pecahan2);
     //tulisan .2 itu buat nentuin ada 2 angka belakang koma
 
     return 0;
